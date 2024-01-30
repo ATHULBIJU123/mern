f08205557658d57a5b435d88948456049c9e7008
ATHULBIJU123 <ak.athulbijukumar@gmail.com>
2024-01-31 03:22:57 +0530

    30Jan

---
+++ JavaScript/Calculator/scriptcalc.js
@@ -0,0 +1,13 @@
+function clear() {
+    document.getElementById("result").value;
+}
+
+function display(value) {
+    document.getElementById("result").value;
+    console.log(value)
+}
+
+function calc(value) {
+    document.getElementById("btn").value;
+    console.log(value)
+}
