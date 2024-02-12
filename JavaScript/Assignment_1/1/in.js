c529d19daa5e50943ae7908b9226cf0ce0437f48
ATHULBIJU123 <ak.athulbijukumar@gmail.com>
2024-02-13 00:30:59 +0530

    f12

--- JavaScript/Assignment_1/1/in.js
+++ JavaScript/Assignment_1/1/in.js
@@ -2,10 +2,10 @@ let btn1 = document.getElementById('b1')
 console.log("Click Bait Button :" ,btn1)
 let p1 = document.getElementsByClassName('p1')
 
-
 console.log("Para1 :" ,p1)
 btn1.addEventListener('click', function(){
-    console.log('Click Event Fired')
+    // console.log('Click Event Fired');
+    p1.textContent = 'Para1 changed';
 })
 
 
---
+++ JavaScript/Assignment_1/2/ind.js
@@ -0,0 +1,20 @@
+//form
+jsonForm = document.getElementById('jsonForm');
+errorMessage = document.getElementById('errorMessage');
+
+jsonForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    //JSON data
+    jsonData = document.getElementById('jsonData').value;
+
+    try {
+        //parsing the JSON data
+        parsedData = JSON.parse(jsonData);
+        errorMessage.textContent = 'not valid';
+        console.log(parsedData);
+    } catch (error) {
+        //  error message
+        errorMessage.textContent = 'Error parsing JSON: ' + error.message;
+    }
+});
