ad1884a8c1a2b469000d77f650cf975e35ce2b04
ATHULBIJU123 <ak.athulbijukumar@gmail.com>
2024-02-20 05:24:19 +0530

    ecommerce

--- JavaScript/XHR/Ecommerece/15feb.js
+++ JavaScript/XHR/Ecommerece/15feb.js
@@ -18,17 +18,29 @@ xhr.onreadystatechange = function () {
             console.log("parsed_result : ",parsed_result);
             // console.log('typeof(parsedresult) :',typeof(parsed_result))
 
-            let container = document.getElementById('container');
-            let box = "";
+            let box = document.getElementById('box');
+            let box1 = " ";
 
             for (let i = 0; i < parsed_result.length; i++) {
-                box = box +" "
+                box1 = box1 + `
+                <div class="box">
+                    <p class="id">${parsed_result[i].id}</p>
+                    <h3 class="title">${parsed_result[i].title}</h3>
+                    <p class="price">${parsed_result[i].price}</p>
+                    <p class="description">${parsed_result[i].description}</p>
+                    <p class="category">${parsed_result[i].category}</p>
+                    <img src="" alt="" class="picture">${parsed_result[i].picture}
+                    <div class="rating">
+                        <p class="rate">${parsed_result[i].rate}</p>
+                        <p class="count">${parsed_result[i].count}</p>
+                    </div>
+                </div>`
                 
             }
 
-
+            console.log("box :",box1)
+            box.innerHTML = box1;
         }
-        console.log("box :",box)
-        container.innerHTML = box;
+
     }
 }
