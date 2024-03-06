416ee13463e269c877371e6550e65ca15f8b8a34
ATHULBIJU123 <ak.athulbijukumar@gmail.com>
2024-03-06 09:28:10 +0530

    server connection to database

--- UMS/server/app.js
+++ UMS/server/app.js
@@ -3,15 +3,15 @@ const url = require('url');
 const port = 3000;
 const fs = require('fs');
 const querystring = require('querystring');
-const { MongoClient} = require('mongodb');
-const {error} = require('console');
+const { MongoClient } = require('mongodb');
+const { error } = require('console');
 
 //Connection URL
 const mongodb_url = 'mongodb://localhost:27017';
 const client = new MongoClient(mongodb_url);
 
 //Database Name
-const dbNAme = 'users';
+const dbName = 'users';
 const collName = 'users_coll';
 
 async function connect() {
@@ -20,10 +20,14 @@ async function connect() {
 
     console.log('Connected successfully to server');
 
+
     return 'done';
 }
 
 const server = http.createServer((req, res) => {
+
+    const db = client.db(dbName);
+    const collection = db.collection(collName);
 
     const req_url = req.url;
     console.log("req_url :", req_url);
@@ -44,7 +48,7 @@ const server = http.createServer((req, res) => {
 
     if (req.method === "POST" && parsed_url.pathname === "/submit") {
 
-        let body = " ";
+        let body = "";
 
         req.on('data', (chunks) => {
             console.log("Chunks :", chunks);
@@ -52,29 +56,51 @@ const server = http.createServer((req, res) => {
             console.log("body : ", body);
         });
 
-        req.on('end', () => {
+        req.on('end', async () => {
             let formDatas = querystring.parse(body);
             console.log("formDatas :", formDatas);
 
             //Do something with the submitted Data ( Eg: Save to a database)
             console.log("Name : ", formDatas.mytext);
-            console.log("Email : ",formDatas.email); http://localhost:3000
+            console.log("Email : ", formDatas.email); http://localhost:3000
             console.log("Password : ", formDatas.pass);
 
-            let response = {
-                success : true,
-                statusCode : 200,
-                data : formDatas,
-                message : "Form Datas submitted Successfully"
-            }
+            //Inserting formDatas to the database
+            await collection.insertOne(formDatas)
+                .then((message) => {
+                    console.log("Document inserted successfully")
 
-            let json_response = JSON.stringify(response);
-            console.log("json_response : ", json_response);
+                    let response = {
+                        success: true,
+                        statusCode: 200,
+                        data: formDatas,
+                        message: "Form Datas submitted Successfully"
+                    }
 
-            res.writeHead(200, {"Content-Type" : "text/json"});
-            res.end(json_response);
+                    let json_response = JSON.stringify(response);
+                    console.log("json_response : ", json_response);
+
+                    res.writeHead(200, { "Content-Type": "text/json" });
+                    res.end(json_response);
+                })
+
+                .catch((error) => {
+                    console.log("Document insertion failed");
+
+                    let response = {
+                        success: false,
+                        statusCode: 400,
+                        data: formDatas,
+                        message: "Failed"
+                    }
+
+                    let json_response = JSON.stringify(response);
+                    console.log("json_response : ", json_response);
+
+                    res.writeHead(200, { "Content-Type": "text/json" });
+                    res.end(json_response);
+                })
         });
-
 
     }
 
@@ -82,14 +108,14 @@ const server = http.createServer((req, res) => {
 
 connect()
     .then((message) => {
-        console.log('message :',message);
+        console.log('message :', message);
     })
     .catch((error) => {
-        console.log("error : ",error);
+        console.log("error : ", error);
     })
     .finally(() => {
         server.listen(port, () => {
             console.log(`Server running at http://localhost:${port}`);
-    })
+        })
 
-});
+    });
